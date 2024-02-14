1cc51939d8aa27e4d077283949f87e0126a58882
Jack Pritom Soren <58485174+jps27CSE@users.noreply.github.com>
2024-02-14 21:16:16 +0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Hey, i am nodejs from container" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
